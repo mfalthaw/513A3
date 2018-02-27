785f351bf7696f3b360e8e1c1af956d49dbf7a3f
Moath <mfalthaw@ucalgary.ca>
2018-02-27 11:39:01 -0800

    log new user

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 var app = require('express')();
 var server = require('http').Server(app);
+var socketIo = require('socket.io')(server);
 
 server.listen('8000');
 
 app.get('/', function(request, response) {
   response.sendFile(__dirname + '/index.html');
 });
+
+
+socketIo.on('connection', function(socket) {
+  console.log('New user connected...');
+})
