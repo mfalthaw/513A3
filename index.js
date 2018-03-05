00eb45930cb243405e377c2bcbd01737a8b5df95
Moath <mfalthaw@ucalgary.ca>
2018-03-05 14:26:35 -0700

    group name

--- index.js
+++ index.js
@@ -10,11 +10,20 @@ app.get('/', function(request, response) {
 
 socketIo.on('connection', function(socket) {
   console.log('New user connected...');
+
+  // newMessage event
   socket.on('newMessage', function (data) {
     // Grab message from client
     console.log('New Message: ' + data);
     // Emit message to all users
     socket.emit('clientMessage', data);
+  });
+
+  // joinGroup event
+  socket.on('joinGroup', function (data) {
+    // Grab group name from client
+    console.log('User joined new group: ' + data);
+    socket.join(data);
   });
 })
 
