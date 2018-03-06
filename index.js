a53efb0e69ea3164799b528c7535c24e56358f36
mfalthaw <mfalthaw@ucalgary.ca>
2018-03-06 00:38:11 -0700

    additions and init chages

--- index.js
+++ index.js
@@ -11,19 +11,26 @@ app.get('/', function(request, response) {
   response.sendFile(__dirname + '/index.html');
 });
 
+var chatHistorySize = 250;
+
 var usersList = {};
+var chatHistory = [];
 
 io.on('connection', function(socket) {
   console.log('New user connected');
 
   // initUser
   socket.on('initUser', function () {
+   
+    // new user
     var username = getUsername();
     usersList[username] = username;
     socket.username = username;
+    console.log('New user created: ' + username);
+
+    // update user list
     io.emit('usersList', usersList);
     io.emit('onlineUsers', socket.conn.server.clientsCount);
-    console.log(username);
   });
 
   // newMessage
@@ -69,5 +76,9 @@ function getTime() {
 
 // return random username
 function getUsername() {
-  return 'user' + Math.floor((Math.random() * 1000) + 1);
+  var temp = 'user' + Math.floor((Math.random() * 1000) + 1);
+  while (usersList[temp]) {
+    temp = 'user' + Math.floor((Math.random() * 1000) + 1);
+  }
+  return temp;
 }
